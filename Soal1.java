94b66e398113924fabaca19c33118ccd8ba7568c
Shellozanof <wntrzanof@gmail.com>
2026-02-12 23:47:17 +0700

    Upload tugas-tugas Java Semester 2

---
+++ Soal1.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Soal1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Print pengulangan berapa kali? ");
+        try {
+            int x = sc.nextInt();
+
+            for (int i = 0; i < x; i++) {
+                System.out.print("\nMasukkan angka (N): ");
+                try {
+                    long n = sc.nextLong();
+                    System.out.println(n + " can be fitted in:");
+
+                    if (n >= Byte.MIN_VALUE && n <= Byte.MAX_VALUE) {
+                        System.out.println("* byte");
+                    }
+                    if (n >= Short.MIN_VALUE && n <= Short.MAX_VALUE) {
+                        System.out.println("* short");
+                    }
+                    if (n >= Integer.MIN_VALUE && n <= Integer.MAX_VALUE) {
+                        System.out.println("* int");
+                    }
+                    if (n >= Long.MIN_VALUE && n <= Long.MAX_VALUE) {
+                        System.out.println("* long");
+                    }
+
+                } catch (Exception e) {
+                    System.out.println(sc.next() + " can't be fitted anywhere.");
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Input pengulangan harus berupa angka!");
+        }
+        
+        sc.close();
+    }
+}
